9e3b9877869b1cd66c04fb6ac708abd7c1455ee2
Jonathan <123389765+NeguseNegest@users.noreply.github.com>
2023-11-04 22:37:06 +0100

    Create TicTacToe.java

---
+++ Lab2/TicTacToe.java
@@ -0,0 +1,70 @@
+//TicTacToe logic
+public class TicTacToe implements Boardgame {
+
+    private String[][] boardArray = new String[3][3];
+    private boolean valid_move; // senaste dragets validitet
+    private boolean user_turn = false;
+
+    public TicTacToe() {
+        fill_squares_empty(); // skapa vårt bord
+    }
+    
+    private void fill_squares_empty() {
+        for (int i = 0; i < boardArray.length; i++) {
+            for (int j = 0; j < boardArray[i].length; j++) {
+                boardArray[i][j] = " ";
+            }
+        }
+    }
+   
+    private boolean _valid_move(int i, int j) {
+        return boardArray[i][j].equals(" "); 
+    }
+
+    public boolean user_turn() {
+        user_turn = !user_turn;
+        return user_turn;
+    }
+    
+    public void swap_signs(int i, int j) {
+        boardArray[i][j] = user_turn ? "X" : "O";
+        user_turn(); 
+    }
+
+    public boolean move(int i, int j) {
+        if (_valid_move(i, j)) {
+            swap_signs(i, j);
+            valid_move = true;
+            getWinner(i,j);
+        } else {
+            valid_move = false;
+        }
+        return valid_move;
+    }
+
+    public String getWinner(int i, int j) {
+        String current_type = boardArray[i][j];
+        
+        if (current_type.equals(boardArray[i][0]) && current_type.equals(boardArray[i][1]) && current_type.equals(boardArray[i][2])) {
+            return current_type + " is the winner";
+
+        } else if (current_type.equals(boardArray[0][j]) && current_type.equals(boardArray[1][j]) && current_type.equals(boardArray[2][j])) {
+
+            return current_type + " is the winner";
+
+        } else if ((current_type.equals(boardArray[0][0]) && current_type.equals(boardArray[1][1]) && current_type.equals(boardArray[2][2])) ||
+                   (current_type.equals(boardArray[0][2]) && current_type.equals(boardArray[1][1]) && current_type.equals(boardArray[2][0]))) {
+            return current_type + " is the winner";
+        }
+        return "No winner yet";
+    }
+    
+
+    public String getStatus(int i, int j) {
+        return boardArray[i][j];
+    }
+
+    public String getMessage() {
+        return valid_move ? "Move accepted." : "Invalid move, try again.";
+    }
+}
