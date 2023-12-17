c288e453a950155b24cbd388898132463c649caa
Jonathan <123389765+NeguseNegest@users.noreply.github.com>
2023-12-17 20:56:36 +0100

    Create Food.java

---
+++ ProjectPacman/Food.java
@@ -0,0 +1,29 @@
+package PacMan;
+
+
+
+public class Food {
+    protected int x;
+    protected int y;
+    protected int smallFood;
+
+
+public void setX(int x){
+    this.x=x;
+}
+
+public void setY(int y){
+    this.y=y;
+}
+
+public int getX(){
+    return x;
+}
+
+public int getY(){
+    return y;
+}
+
+
+
+}
