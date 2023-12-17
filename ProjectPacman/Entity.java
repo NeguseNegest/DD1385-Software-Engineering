3baa1faae68750defda2016181d7c43ce101bd86
Jonathan <123389765+NeguseNegest@users.noreply.github.com>
2023-12-17 20:56:57 +0100

    Create Entity.java

---
+++ ProjectPacman/Entity.java
@@ -0,0 +1,38 @@
+package PacMan;
+
+public abstract class Entity {
+	protected int x;
+	protected int y;
+	protected int state = 1;
+	protected boolean panick;
+	
+	public int getX() {
+		return x;
+	}
+	public void setX(int x) {
+		this.x = x;
+	}
+	public int getY() {
+		return y;
+	}
+	public void setY(int y) {
+		this.y = y;
+	}
+	public void setState(int state){
+		this.state = state;
+	}
+
+	public int getState(){
+		return state;
+	}
+
+	public void getEmotion(){
+		this.panick=panick;
+		
+	}
+
+	public boolean bePanick(){
+		return panick=true;
+	}
+	
+}
