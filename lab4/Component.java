6a68a79f65b3f9737f2bb8c1ee29b109af12b934
Lucas <l.m.frykman@gmail.com>
2024-01-29 18:20:58 +0100

    removing forked files

--- lab4/Component.java
+++
@@ -1,8 +0,0 @@
-package lab4;
-public interface Component{
-    int getWeight();
-    void add(Component component);
-    void remove(Component component);
-    Component getChild(int index);
-    String toString(); // Operation method
-}
